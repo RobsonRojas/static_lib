10f4e199304933333eed557020b02bc6e1ad0c36
robson.rojas <robson.rojasa@gmail.com>
2022-08-27 17:02:39 -0300

    base commit

---
+++ static_lib/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "static_lib.h"
+
+int teste() {
+	printf("teste\n");
+	return 0;
+}
---
+++ use_static_lib/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "static_lib.h"
+
+int main(void) {
+	teste();
+	printf("main\n");
+	return 0;
+}
